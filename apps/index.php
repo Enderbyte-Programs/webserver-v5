152f76360c3a0a21e746301d90a6e41cda06034a
BuildTools <unconfigured@null.spigotmc.org>
2024-02-13 20:07:50 -0800

    autoupdateindex

---
+++ apps/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE php>
+<html lang="en">
+<head>
+    <script src="/lib/jquery.js"></script> 
+    <script> 
+        $(function(){
+          $("#includedContent").load("/apps/head.html"); 
+        });
+        </script> 
+    <!--Import header using Jquery-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Apps</title>
+</head>
+<body>
+    <div id="includedContent"></div>
+    <!--Remember! Copy this-->
+    <h1 style="text-align: center;">My Web Apps</h1>
+    <p style="text-align: center;">Here are some web apps I have created. I mainly dabble in desktop software, so these are all pretty bad.</p>
+    <hr />
+    <p style="text-align: center;"><strong>The Big List:</strong></p>
+    <ul>
+    <li style="text-align: left;">Teacher Ratings: Rate your teachers. 
+            <a title="Go to Teacher Ratings" href="http://enderbyteprograms.bsite.net" target="_blank" rel="noopener">CLICK HERE
+
+            </a>
+        </li>
+        <?php
+            function splitlines($ar) {
+                return preg_split("/\r\n|\n|\r/", $ar);
+            }
+
+            $subdirs = array();
+            $items = glob("/home/jordan/master_web/apps" . '/*');
+            foreach ($items as $item) {
+                
+                if (is_dir($item)) {
+                    array_push($subdirs,$item);
+                }
+            }
+            foreach ($subdirs as $subdir) {
+                $spath = $subdir . "/.epapp";
+                if (is_file($spath)) {
+                    $file = fopen($spath,"r");
+                    $data = fread($file,filesize($spath));
+                    fclose($file);
+
+                    //Now process
+                    $procdata = splitlines($data);
+                    $title = $procdata[0];
+                    $description = $procdata[1];
+                    $strippedpath = str_replace("/home/jordan/master_web","",$subdir);
+
+                    $inspath = $subdir . "/index.html";
+                    echo sprintf("<li style=\"text-align: left;\">%s: %s <a 
+                    title=\"Go to that web app\" href=\"%s\">CLICK HERE
+                </a></li>",$title,$description,$strippedpath);
+                }
+            }
+        ?>
+    </ul>
+</body>
+</html>
