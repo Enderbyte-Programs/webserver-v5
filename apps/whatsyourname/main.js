48911b83b4d7ed4a2cd824a0d1c73568ced015e8
BuildTools <unconfigured@null.spigotmc.org>
2024-02-13 18:57:48 -0800

    headers with jquery

--- apps/whatsyourname/main.js
+++ apps/whatsyourname/main.js
@@ -3,3 +3,8 @@ function myf() {
     alert("Hello, ".concat(name,"!"));
     return false;
 }
+
+function processcl() {
+    var cl = document.getElementsByName('cl')[0].value;
+    document.body.style.background = cl;
+}
